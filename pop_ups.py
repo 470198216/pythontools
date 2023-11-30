b6df7f46d81cefae2784113210d2594bf9336654
mh5wl5wf <470198216@qq.com>
2023-11-30 17:04:19 +0800

    update receive func

--- pop_ups.py
+++ pop_ups.py
@@ -1,3 +1,25 @@
 import win32api
+from socket import socket, AF_INET, SOCK_STREAM
+def pop_ups(message):
+    win32api.MessageBox(0, message, "tips")
 
-win32api.MessageBox(0, "wwj test", "tips")
+def echo_handle(address, client_sock):
+    print("Got connect from {}".format(address))
+    while True:
+        msg = client_sock.recv(8192)
+        if not msg:
+            break
+        client_sock.sendall(msg)
+        pop_ups(msg.decode('utf-8'))
+    client_sock.close()
+
+def echo_server(address, backlog=5):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(backlog)
+    while True:
+        client_sock, client_addr = sock.accept()
+        echo_handle(client_addr, client_sock)
+
+if __name__ == '__main__':
+    echo_server(('', 20000))
